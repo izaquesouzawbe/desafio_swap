7301b44d9a54d3bf4431f337db08ed3b321b8db9
izaque <izaquesouzawbe@gmail.com>
2022-11-29 19:00:47 -0300

    Projeto inicial

---
+++ src/main/java/com/desafioswap/webhook/WebhookApplication.java
@@ -0,0 +1,13 @@
+package com.desafioswap.webhook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebhookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebhookApplication.class, args);
+	}
+
+}
